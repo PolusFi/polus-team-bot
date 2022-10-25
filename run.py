6772ce9d4bcc39da84d09e1179b82b018c40098d
ArtemBon <artembalitskyi@gmail.com>
2022-10-26 00:42:24 +0300

    scheduler run

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-from router import app
+import asyncio
 import os
+
+from router import app
+from scheduler import meeting_notification
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv('PORT', default=8000), )
+    asyncio.run(meeting_notification())
