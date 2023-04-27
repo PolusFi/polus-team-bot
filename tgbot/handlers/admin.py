24096e3f241addc860d87f9c3110da7e2a77bf9f
Артем <50744314+ArtemBon1@users.noreply.github.com>
2023-04-27 18:17:51 +0300

    Update admin.py

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -106,11 +106,11 @@ async def admin_notify_group(callback_query: CallbackQuery, callback_data: dict)
                      f'📈 Цель: {meeting_doc["goal"]}\n\n' \
                      f'📆 Дата: {meeting_doc["date"].strftime("%d/%m/%Y")}\n' \
                      f'⏰ Time:\n' \
-                     f'Istanbul: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                     f'Kyiv: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                     f'Batumi: {0 if int(hour)+1 < 10}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
-                     f'Almaty: {0 if int(hour)+3 < 10}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
-                     f'EST: {0 if int(hour)-7 < 10}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n'
+                     f'Istanbul: {0 if int(hour) < 10 else ""}{int(hour)}:{minute} ({0 if int(hour) < 10 else ""}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                     f'Kyiv: {0 if int(hour) < 10 else ""}{int(hour)}:{minute} ({0 if int(hour) < 10 else ""}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                     f'Batumi: {0 if int(hour)+1 < 10 else ""}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10 else ""}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
+                     f'Almaty: {0 if int(hour)+3 < 10 else ""}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10 else ""}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
+                     f'EST: {0 if int(hour)-7 < 10 else ""}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10 else ""}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n'
 
         try:
             await callback_query.bot.send_message(chat_id=member['telegram_id'],
@@ -126,11 +126,11 @@ async def admin_notify_group(callback_query: CallbackQuery, callback_data: dict)
               f'📆 Date: {meeting_doc["date"].strftime("%d/%m/%Y")}\n' \
               f'⏰ Time:\n' \
               f'Istanbul: {hour}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-              f'Istanbul: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-              f'Kyiv: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-              f'Batumi: {0 if int(hour)+1 < 10}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
-              f'Almaty: {0 if int(hour)+3 < 10}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
-              f'EST: {0 if int(hour)-7 < 10}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n' \
+              f'Istanbul: {0 if int(hour) < 10 else ""}{int(hour)}:{minute} ({0 if int(hour) < 10 else ""}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+              f'Kyiv: {0 if int(hour) < 10 else ""}{int(hour)}:{minute} ({0 if int(hour) < 10 else ""}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+              f'Batumi: {0 if int(hour)+1 < 10 else ""}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10 else ""}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
+              f'Almaty: {0 if int(hour)+3 < 10 else ""}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10 else ""}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
+              f'EST: {0 if int(hour)-7 < 10 else ""}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10 else ""}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n' \
               f'👥 Members: \n{members}'
 
     msg = await callback_query.bot.send_message(chat_id=callback_query.bot['config'].tg_bot.dev_chat,
