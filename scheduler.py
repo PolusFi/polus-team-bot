2ca311c9b8895f459caa564f686207e7ec9ecc9b
Артем <50744314+ArtemBon1@users.noreply.github.com>
2023-04-27 18:11:38 +0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -32,7 +32,7 @@ async def meeting_notification():
             }
             current_date = datetime.now() + timedelta(hours=3, minutes=30)
             current_date_5 = datetime.now() + timedelta(hours=3, minutes=5)
-            hour = meeting_doc["time"].split(":")[0]
+            hour = int(meeting_doc["time"].split(":")[0])
             minute = meeting_doc["time"].split(":")[1]
             if meeting_date.year == current_date.year and \
                     meeting_date.month == current_date.month and \
@@ -58,11 +58,11 @@ async def meeting_notification():
                                  f'📈 Цель: {meeting_doc["goal"]}\n\n' \
                                  f'📆 Дата: {meeting_doc["date"].strftime("%d/%m/%Y")}\n' \
                                  f'⏰ Время:\n' \
-                                 f'Istanbul: {hour}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                                 f'Kyiv: {int(hour)}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                                 f'Batumi: {int(hour)+1}:{minute} ({(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
-                                 f'Almaty: {int(hour)+3}:{minute} ({(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
-                                 f'EST: {int(hour)-8}:{minute} ({(int(hour)-8) % 12}:{minute} {"PM" if (hour-8) > 12 else "AM"})\n'
+                                 f'Istanbul: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                                 f'Kyiv: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                                 f'Batumi: {0 if int(hour)+1 < 10}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
+                                 f'Almaty: {0 if int(hour)+3 < 10}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
+                                 f'EST: {0 if int(hour)-7 < 10}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n'
                     try:
                         await bot.send_message(chat_id=member['telegram_id'],
                                                text=remind_msg,
@@ -76,11 +76,11 @@ async def meeting_notification():
                           f'📈 Object: {meeting_doc["goal"]}\n\n' \
                           f'📆 Date: {meeting_doc["date"].strftime("%d/%m/%Y")}\n' \
                           f'⏰ Time:\n' \
-                          f'Istanbul: {hour}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                          f'Kyiv: {int(hour)}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                          f'Batumi: {int(hour)+1}:{minute} ({(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
-                          f'Almaty: {int(hour)+3}:{minute} ({(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
-                          f'EST: {int(hour)-8}:{minute} ({(int(hour)-8) % 12}:{minute} {"PM" if (hour-8) > 12 else "AM"})\n\n' \
+                          f'Istanbul: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                          f'Kyiv: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                          f'Batumi: {0 if int(hour)+1 < 10}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
+                          f'Almaty: {0 if int(hour)+3 < 10}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
+                          f'EST: {0 if int(hour)-7 < 10}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n' \
                           f'👥 Members: \n{members}'
 
                 msg = await bot.send_message(chat_id=config.tg_bot.dev_chat,
@@ -123,11 +123,11 @@ async def meeting_notification():
                           f'📈 Object: {meeting_doc["goal"]}\n\n' \
                           f'📆 Date: {meeting_doc["date"].strftime("%d/%m/%Y")}\n' \
                           f'⏰ Time:\n' \
-                          f'Istanbul: {hour}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                          f'Kyiv: {int(hour)}:{minute} ({hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
-                          f'Batumi: {int(hour)+1}:{minute} ({(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
-                          f'Almaty: {int(hour)+3}:{minute} ({(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
-                          f'EST: {int(hour)-8}:{minute} ({(int(hour)-8) % 12}:{minute} {"PM" if (hour-8) > 12 else "AM"})\n\n' \
+                          f'Istanbul: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                          f'Kyiv: {0 if int(hour) < 10}{int(hour)}:{minute} ({0 if int(hour) < 10}{hour % 12}:{minute} {"PM" if hour > 12 else "AM"})\n' \
+                          f'Batumi: {0 if int(hour)+1 < 10}{int(hour)+1}:{minute} ({0 if int(hour)+1 < 10}{(hour+1) % 12}:{minute} {"PM" if (hour+1) > 12 else "AM"})\n' \
+                          f'Almaty: {0 if int(hour)+3 < 10}{int(hour)+3}:{minute} ({0 if int(hour)+3 < 10}{(hour+3) % 12}:{minute} {"PM" if (hour+3) > 12 else "AM"})\n' \
+                          f'EST: {0 if int(hour)-7 < 10}{int(hour)-7}:{minute} ({0 if int(hour)-7 < 10}{(int(hour)-7) % 12}:{minute} {"PM" if (hour-7) > 12 else "AM"})\n\n' \
                           f'👥 Members: \n{members}'
 
                 await bot.send_message(chat_id=config.tg_bot.dev_chat,
